2aa52bbcdbfa28facd63a41b353b23171e36067a
AlfonsoBarguno <caballerotito@yahoo.es>
2022-11-30 16:22:20 +0100

    30/11

--- src/main/java/com/ironLibrary/IronLibrary/Library/Library.java
+++ src/main/java/com/ironLibrary/IronLibrary/Library/Library.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -31,24 +33,20 @@ public class Library {
     IssueRepository issueRepository;
 
 
-
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     Scanner scanner = new Scanner(System.in);
     Book book1 = new Book();
-    Student student1= new Student();
+    Student student1 = new Student();
     Issue issue1 = new Issue();
     Author author1 = new Author();
 
 
-
-
-
     public Library(AuthorRepository authorRepository1, StudentRepository studentRepository1, BookRepository bookRepository1, IssueRepository issueRepository1) {
 
-        authorRepository=authorRepository1;
-        studentRepository= studentRepository1;
-        bookRepository=bookRepository1;
-        issueRepository=issueRepository1;
+        authorRepository = authorRepository1;
+        studentRepository = studentRepository1;
+        bookRepository = bookRepository1;
+        issueRepository = issueRepository1;
     }
 
     public void addBookData() throws IOException {
@@ -74,18 +72,19 @@ public class Library {
         Enter number of books : 4
  */
     }
+
     public void createAuthor() throws IOException {
         System.out.println("Enter author name: ");
         String authorName = input.readLine();
-        if (authorRepository.findByName(authorName).isPresent()){
+        if (authorRepository.findByName(authorName).isPresent()) {
             author1 = authorRepository.findByName(authorName).get();
             book1.setAuthor(author1);
             // añadir el libro a la lista del autor
-        }else{
+        } else {
             //en caso de no encontrar el Author en la base de datos, creamos uno nuevo
             System.out.println("Enter author email: ");
             String authorEmail = input.readLine();
-            Author newAuthor = new Author(authorName, authorEmail,new ArrayList<>());
+            Author newAuthor = new Author(authorName, authorEmail, new ArrayList<>());
             authorRepository.save(newAuthor);
             book1.setAuthor(newAuthor);
             // añadir el libro a la lista del autor
@@ -97,16 +96,15 @@ public class Library {
     }
 
 
-
     public void searchBookByTitle() throws IOException {
 
         System.out.println("Enter title: ");
         String title = input.readLine();
-        if(bookRepository.findBookByTitle(title).isPresent()){
+        if (bookRepository.findBookByTitle(title).isPresent()) {
             book1 = bookRepository.findBookByTitle(title).get();
             System.out.println("Book ISBN        Book Title       Category       Number of Books");
             System.out.println(book1.getIsbn() + "     " + " " + book1.getTitle()
-                    +"       " + "    " +book1.getCategory() + "          " + "    " + book1.getQuantity());
+                    + "       " + "    " + book1.getCategory() + "          " + "    " + book1.getQuantity());
         } else System.out.println("No book with this title.");
     }
 
@@ -115,29 +113,28 @@ public class Library {
         System.out.println("Enter category: ");
         String category = input.readLine();
         List<Book> bookList = bookRepository.findBookByCategory(category);
-        for(int i=0; i< bookList.size();i++){
-            if(bookList.get(i).getCategory().equalsIgnoreCase(category)){
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getCategory().equalsIgnoreCase(category)) {
                 System.out.println("Book ISBN        Book Title       Category       Number of Books");
                 System.out.println(bookList.get(i).getIsbn() + "     " + " " + bookList.get(i).getTitle()
-                        +"       " + "    " +bookList.get(i).getCategory() + "          " + "    " + bookList.get(i).getQuantity());
+                        + "       " + "    " + bookList.get(i).getCategory() + "          " + "    " + bookList.get(i).getQuantity());
             } else System.out.println("No book in this category.");
-            }
         }
-
+    }
 
 
     public void searchBookByAuthor() throws IOException {
 
         System.out.println("Enter author: ");
         String authorName = input.readLine();
-        if(authorRepository.findByName(authorName).isPresent()){
-            author1=authorRepository.findByName(authorName).get();
-        }else System.out.println("No book with this author.");
-        if(bookRepository.findBookByAuthor(author1).isPresent()){
+        if (authorRepository.findByName(authorName).isPresent()) {
+            author1 = authorRepository.findByName(authorName).get();
+        } else System.out.println("No book with this author.");
+        if (bookRepository.findBookByAuthor(author1).isPresent()) {
             book1 = bookRepository.findBookByAuthor(author1).get();
             System.out.println("Book ISBN        Book Title       Category       Number of Books");
             System.out.println(book1.getIsbn() + "     " + " " + book1.getTitle()
-                    +"       " + "    " +book1.getCategory() + "          " + "    " + book1.getQuantity());
+                    + "       " + "    " + book1.getCategory() + "          " + "    " + book1.getQuantity());
         }
     }
 
@@ -145,17 +142,69 @@ public class Library {
 
         List<Book> bookList = bookRepository.findAll();
 
-        for(int i =0; i < bookList.size(); i++){
+        for (int i = 0; i < bookList.size(); i++) {
             System.out.println("Book ISBN        Book Title       Category       Number of Books");
             System.out.println(bookList.get(i).getIsbn() + "     " + " " + bookList.get(i).getTitle()
-                    +"       " + "    " +bookList.get(i).getCategory() + "          " + "    " + bookList.get(i).getQuantity());
+                    + "       " + "    " + bookList.get(i).getCategory() + "          " + "    " + bookList.get(i).getQuantity());
 
         }
     }
 
-    public void issueBookToStudent() {
+    public void issueBookToStudent() throws IOException {
+
+        System.out.println("Enter the student's USN");
+        String usn = input.readLine();
+        student1.setUsn(usn);
+        System.out.println("Enter the name");
+        String name = input.readLine();
+        student1.setName(name);
+        studentRepository.save(student1);
+        System.out.println("The student data have been saved");
+
+        boolean isbnTrue = true;
+        do {
+            System.out.println("Please enter the book ISBN: ");
+            String bookIsbn = input.readLine();
+            boolean bookId = bookRepository.findById(bookIsbn).isPresent();
+            if (bookId == true) {
+                issue1.setIssueBook(bookRepository.findById(bookIsbn).get());
+                issue1.setIssueStudent(studentRepository.findById(usn).get());
+                LocalDateTime dateTime = LocalDateTime.now();
+                issue1.setIssueDate(String.valueOf(dateTime));
+                LocalDate returnDate = LocalDate.now().plusDays(7);
+                issue1.setReturnDate(String.valueOf(returnDate));
+                issueRepository.save(issue1);
+                System.out.println("Book issued. Return date: " + returnDate);
+                isbnTrue = false;
+            } else {
+                System.out.println("Please, insert a valid ISBN.");
+                //debería volver al menú principal o repreguntar?
+            }
+        } while (isbnTrue);
+
+
     }
 
-    public void listBooksByUsn() {
+    public void listBooksByUsn() throws IOException {
+
+        System.out.println("Enter the student's USN: ");
+        String usn = input.readLine();
+
+        if(studentRepository.findById(usn).isPresent()){
+
+            student1 = studentRepository.findById(usn).get();
+        }
+
+        if (issueRepository.findByIssueStudent(student1).isPresent()) {
+
+            issue1 = issueRepository.findByIssueStudent(student1).get();
+
+            System.out.println("Book Title        Student Name       Return Date");
+            System.out.println(issue1.getIssueBook().getTitle() + "    " + issue1.getIssueStudent().getName() + "    " + issue1.getReturnDate());
+
+        } else throw new IllegalArgumentException("No usn valid.");
     }
 }
+
+
+
